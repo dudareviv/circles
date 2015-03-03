c8d7051a954b09356c33fd2db83bbcfdfddc3ac8
Dudarev Ilya <dudareviv@gmail.com>
2015-03-03 17:41:32 +0300

    Remove debug.

--- Assets/Scripts/Behaviors/Circle.cs
+++ Assets/Scripts/Behaviors/Circle.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class Circle : MonoBehaviour
 {
+	/**
+	 * Задает цвет для спрайта.
+	 */
 	void SetColor (Color color) {
 		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer> ();
 		spriteRenderer.color = color;
 	}
 
-	void OnMouseDown() {
-		Debug.Log ("Clicked");
-		Poof ();
-	}
-
+	/**
+	 * Уничтожает объект и сообщаем об этом.
+	 */
 	void Poof(){
 		EventManager.CirclePoof.Publish (this);
 		Destroy (gameObject);
